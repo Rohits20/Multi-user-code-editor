05abdbd07a2bf0ba345c475f70bc6146aecb5c4a
Rohits20 <rh.rohit2020@gmail.com>
2022-06-26 16:20:08 +0530

    fixed error

--- server.js
+++ server.js
@@ -8,11 +8,12 @@ const ACTIONS = require("./src/Actions");
 const server = http.createServer(app);
 const io = new Server(server);
 
+app.use(express.static("build"));
 app.use((req, res, next) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.use(express.static("build"));
+
 
 const userSocketMap = {};
 
